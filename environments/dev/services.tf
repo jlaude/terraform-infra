d9e999a2f0363c64e0cfdf2f57d4986cb10bf98c
John Laude <admin@jlaude.altostrat.com>
2022-11-21 20:53:52 +0000

    removed unnecessary files

--- environments/dev/services.tf
+++
@@ -1,5 +0,0 @@
-resource "google_project_service" "gcp_services" {
-  for_each = toset(var.gcp_service_list)
-  project  = var.project_id
-  service  = each.key
-}
--- environments/staging/backend.tf
+++
@@ -1,6 +0,0 @@
-terraform {
-  backend "gcs" {
-    bucket = "0efe1a640c36e423-bucket-tfstate"
-    prefix = "terraform/state/staging"
-  }
-}
