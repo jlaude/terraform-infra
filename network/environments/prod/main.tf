54fe30c5efaeb926f0be2bcee383d41222936e28
John Laude <admin@jlaude.altostrat.com>
2022-11-21 21:50:06 +0000

    added cloud-nat

--- network/environments/prod/main.tf
+++ network/environments/prod/main.tf
@@ -27,3 +27,20 @@ module "vpc" {
   secondary_ranges = var.secondary_ranges
 
 }
+
+resource "google_compute_router" "router" {
+  project = local.project_id
+  name    = "nat-router"
+  network = "jlaude-labs-${local.environment}-vpc"
+  region  = "us-central1"
+}
+
+module "cloud-nat" {
+  source                             = "terraform-google-modules/cloud-nat/google"
+  version                            = "2.2.1"
+  project_id                         = local.project_id
+  region                             = "us-central1"
+  router                             = google_compute_router.router.name
+  name                               = "nat-config"
+  source_subnetwork_ip_ranges_to_nat = "ALL_SUBNETWORKS_ALL_IP_RANGES"
+}
